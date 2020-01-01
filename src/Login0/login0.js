c3fc818744c737347fc8b22330bc19dfe6082f21
harrylip <3121630091@qq.com>
2020-01-01 19:23:11 +0800

    更新

--- src/Login0/login0.js
+++ src/Login0/login0.js
@@ -1,59 +1,48 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
-import { Form, Icon, Input, Button, Checkbox } from 'antd'
+import { Form, Icon, Input, Button, Checkbox,message } from 'antd'
 var Login0Css =require('./login0.css')
 
-function hasErrors(fieldsError) {
-    return Object.keys(fieldsError).some(field => fieldsError[field]);
-}
+
   
 
- class Login extends React.Component{
-    componentDidMount() {
-        // To disabled submit button at the beginning.
-        this.props.form.validateFields();
-      }
+export default class Login extends React.Component{
     
-    handleSubmit = e =>{
-        e.preventDefault();
-        this.props.form.validateFields((err,value) => {
-            if(!err){
-                console.log('Received value of form', value);
-            }
-        });
-    };
 
     constructor(props){
         super(props);
-        this.state={
-            username:'',
-            password:''
-        }
-    }
-    changeValue= e =>{
+        this.state={}
+      }
+      changeValue=(e)=>{
         this.setState({
-            [e.target.name]:e.target.value
+          [e.target.name]:e.target.value
         })
-    }
-    homepage = e =>{
-        if(this.state.username=='景轩'&&this.state.password=='123456'){
-            alert('登录成功');
-            window.localStorage.setItem("username","景轩")
-            window.localStorage.setItem("token","111111")
-            //跳转首页
-            this.props.history.push('/homepage')
-        }else{
-            alert("登录失败");
+      }
+      login = () =>{
+        var data = {
+          "username":this.state.username,
+          "password":this.state.password,
         }
+        fetch("/user/login",{
+          method:"post",
+          headers:{
+            "Content-Type":"application/json"
+          },
+          body:JSON.stringify(data)
+        }).then(response=>response.json())
+        .then(result=>{
+          if(result.state==2){
+            message.info("登录成功")
+            this.props.history.push('/homepage')
+          }else if(result.state==1){
+            message.info("登录失败")
+          }
+        })
+      }
 
-    }
+    
 
     render(){
-        const { getFieldDecorator,getFieldsError, getFieldError, isFieldTouched } = this.props.form;
-
-    // Only show error after a field is touched.
-    const usernameError = isFieldTouched('username') && getFieldError('username');
-    const passwordError = isFieldTouched('password') && getFieldError('password');
         return(
                 <div className={Login0Css.main}>
                     <div className={Login0Css.top}>
@@ -63,34 +52,26 @@ function hasErrors(fieldsError) {
                         <Link to="/register" className={Login0Css.bu1}>注册</Link>
                         <img className={Login0Css.top3} src="ph1.png" width="200px" height="140px"></img>
                     </div>
-                    <div className={Login0Css.img}>
+                    <div className={Login0Css.mid}>
+                        <div className={Login0Css.form1}>
+                            <p className={Login0Css.p}>登录</p>
+                        <div className={Login0Css.form2}>
+                            <Form.Item>
+                                <Input type="username" name="username" value={this.state.username} onChange={e=>this.changeValue(e)}  placeholder="用户名" style={{ height: '40px' ,width:'280px'}} />
+                            </Form.Item>
+                            <Form.Item>
+                                <Input.Password type="password" name="password" value={this.state.password} onChange={e=>this.changeValue(e)} placeholder="密码" style={{ height: '40px' ,width:'280px'}} />
+                            </Form.Item>
+                            <Button className={Login0Css.bu2} onClick={this.login} style={{ background:'rgb(141, 182, 230)', height: '40px' ,width:'200px',fontSize:'20px'}}>登录</Button>
+                        </div>
+                        </div>
                         <div className={Login0Css.title1}>致力于打造互联网</div>
                         <div className={Login0Css.title2}>茶文化优质社区</div>
                         <img  className={Login0Css.ph2} src="ph2.png" width="360px" height="340px"></img>
                     </div>
-                    <Form layout="inline" onSubmit={this.handleSubmit} className={Login0Css.form}>
-                        登录
-                        <Form.Item validateStatus={usernameError ? 'error' : ''} help={usernameError || ''} className={Login0Css.item}>
-                        {getFieldDecorator('username', {rules: [{ required: true, message: '请输入用户名' }],})(
-                            <Input name="username"id="username" className={Login0Css.input} value={this.state.username} onChange={this.changeValue} prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />}placeholder="Username"/>, )}
-                        </Form.Item>
-                        <Form.Item validateStatus={passwordError ? 'error' : ''} help={passwordError || ''} className={Login0Css.item}>
-                        {getFieldDecorator('password', { rules: [{ required: true, message: '请输入密码!' }], })(
-                            <Input name="password"id="password" className={Login0Css.input} value={this.state.password} onChange={this.changeValue} prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />} type="password"placeholder="Password"/>, )}
-                        </Form.Item>
-                        <Form.Item className={Login0Css.item}>
-                        {getFieldDecorator('remember', {valuePropName: 'checked', initialValue: true,})(<Checkbox>记住密码</Checkbox>)}
-                        <Link to="/forget">忘记密码</Link> Or <Link to="/register">注册</Link>
-                        </Form.Item>
-                        <Form.Item className={Login0Css.item}> 
-                                <Button onClick={this.homepage} value="登录" type="primary" htmlType="submit" disabled={hasErrors(getFieldsError())} className={Login0Css.button}>
-                                   登录
-                                </Button>
-                        </Form.Item>
-                    </Form>
                 </div>
         )
     }
 }
-const WrappedNormalLoginForm = Form.create({ name: 'horizontal_login' })(Login);
-export default WrappedNormalLoginForm
+// const WrappedNormalLoginForm = Form.create({ name: 'horizontal_login' })(Login);
+// export default WrappedNormalLoginForm
--- src/Register/register.js
+++ src/Register/register.js
@@ -1,162 +1,63 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
-import {  Form, Input, Select,   Checkbox,  Button, AutoComplete,Popconfirm, message
-  } from 'antd';
+import { Form, Icon, Input, Button, Checkbox,Message, message } from 'antd';
+import ColumnGroup from 'antd/lib/table/ColumnGroup';
+import Axios from 'axios';
 var RegisterCss =require('./register.css')
-const { Option } = Select;
-const AutoCompleteOption = AutoComplete.Option;
 
-function hasErrors(fieldsError) {
-  return Object.keys(fieldsError).some(field => fieldsError[field]);
-}
+export default class Register extends React.Component{
 
-var RegisterCss =require('./register.css')
-    function confirm(e) {
-        console.log(e);
-        message.success('注册成功！');
+  constructor(props){
+    super(props);
+    this.state={}
+  }
+  changeValue=(e)=>{
+    this.setState({
+      [e.target.name]:e.target.value
+    })
+  }
+  register = () =>{
+    var data = {
+      "username":this.state.username,
+      "password":this.state.password,
+      "phonenumber":this.state.phonenumber
     }
-    
-    function cancel(e) {
-        console.log(e);
-        message.error('注册失败!');
-    }
+    fetch("/user/register",{
+      method:"post",
+      headers:{
+        "Content-Type":"application/json"
+      },
+      body:JSON.stringify(data)
+    }).then(response=>response.json())
+    .then(result=>{
+      if(result.state==2){
+        message.info("用户名已存在")
+        this.props.history.push('/login0')
+      }else if(result.state==1){
+        message.info("注册成功")
+      }
+    })
+  }
 
-
- class Register extends React.Component{
-    state = {
-        confirmDirty: false,
-        autoCompleteResult: [],
-      };
-      
-      handleSubmit = e => {
-        e.preventDefault();
-        this.props.form.validateFieldsAndScroll((err, values) => {
-          if (!err) {
-            console.log('Received values of form: ', values);
-          }
-        });
-      };
-    
-      handleConfirmBlur = e => {
-        const { value } = e.target;
-        this.setState({ confirmDirty: this.state.confirmDirty || !!value });
-      };
-    
-      compareToFirstPassword = (rule, value, callback) => {
-        const { form } = this.props;
-        if (value && value !== form.getFieldValue('password')) {
-          callback('您输入的两次密码不一致!');
-        } else {
-          callback();
-        }
-      };
-    
-      validateToNextPassword = (rule, value, callback) => {
-        const { form } = this.props;
-        if (value && this.state.confirmDirty) {
-          form.validateFields(['confirm'], { force: true });
-          this.props.history.push('/homepage')
-        }
-        callback();
-      };
-    
-      handleWebsiteChange = value => {
-        let autoCompleteResult;
-        if (!value) {
-          autoCompleteResult = [];
-        } else {
-          autoCompleteResult = ['.com', '.org', '.net'].map(domain => `${value}${domain}`);
-        }
-        this.setState({ autoCompleteResult });
-      };
-    
-    render(){
-        const { getFieldDecorator ,getFieldsError} = this.props.form;
-        const { autoCompleteResult } = this.state;
-    
-        const formItemLayout = {
-          labelCol: {
-            xs: { span: 24 },
-            sm: { span: 8 },
-          },
-          wrapperCol: {
-            xs: { span: 24 },
-            sm: { span: 16 },
-          },
-        };
-        const tailFormItemLayout = {
-          wrapperCol: {
-            xs: {
-              span: 24,
-              offset: 0,
-            },
-            sm: {
-              span: 16,
-              offset: 8,
-            },
-          },
-        };
-    
-        const websiteOptions = autoCompleteResult.map(website => (
-          <AutoCompleteOption key={website}>{website}</AutoCompleteOption>
-        ));
-          
-        return(
-            <div className={RegisterCss.background}>
-                <div  className={RegisterCss.kuang}>
-                <img className={RegisterCss.logo} src="logo2.png" width="250px" height="110px"></img>
-                <Form {...formItemLayout} onSubmit={this.handleSubmit} className={RegisterCss.form}>
-                        <Form.Item
-                        label={
-                            <span>
-                              用户名&nbsp;
-                            </span>}
-                        >
-                        {getFieldDecorator('username', {
-                            rules: [{ required: true, message: '请填写您的用户名!', whitespace: true }],
-                        })(<Input style={{ height: '40px' }}/>)}
-                        </Form.Item>
-                        <Form.Item label="密码" hasFeedback>
-                        {getFieldDecorator('password', {
-                            rules: [
-                            {
-                                required: true,
-                                message: '请输入您的密码!',
-                            },
-                            {
-                                validator: this.validateToNextPassword,
-                            },
-                            ],
-                        })(<Input.Password  style={{ height: '40px' }} />)}
-                        </Form.Item>
-                        <Form.Item label="确认密码" hasFeedback>
-                        {getFieldDecorator('confirm', {
-                            rules: [
-                            {
-                                required: true,
-                                message: '请确认您的密码!',
-                            },
-                            {
-                                validator: this.compareToFirstPassword,
-                            },
-                            ],
-                        })(<Input.Password style={{ height: '40px' }} onBlur={this.handleConfirmBlur} />)}
-                        </Form.Item>
-                        <Form.Item label="电话">
-                        {getFieldDecorator('phone', {
-                            rules: [{ required: true, message: '请输入您的电话号码!' }],
-                        })(<Input  style={{ height: '40px' }} />)}
-                        </Form.Item> 
-                        <Form.Item> 
-                                <Button onClick={this.homepage} value="登录" type="primary" htmlType="submit" disabled={hasErrors(getFieldsError())} className={RegisterCss.button}>
-                                   注册
-                                </Button>
-                        </Form.Item>
-                </Form> 
-                </div>
+  render(){
+    return(
+      <div className={RegisterCss.background}>
+            <div  className={RegisterCss.kuang}>
+            <img className={RegisterCss.logo} src="logo2.png" width="250px" height="110px"></img>
+            <div className={RegisterCss.form}>
+                <Form.Item>
+                    <Input type="username" name="username" value={this.state.username} onChange={e=>this.changeValue(e)}  placeholder="用户名" style={{ height: '40px' ,width:'300px'}} />
+                </Form.Item>
+                <Form.Item>
+                    <Input.Password type="password" name="password" value={this.state.password} onChange={e=>this.changeValue(e)} placeholder="密码" style={{ height: '40px' ,width:'300px'}} />
+                </Form.Item>
+                <Form.Item>
+                    <Input  type="phonenumber" name="phonenumber" value={this.state.phonenumber} onChange={e=>this.changeValue(e)}  placeholder="电话" style={{ height: '40px' ,width:'300px'}}/>
+                </Form.Item>
+                <Button onClick={this.register} style={{ background:'rgb(141, 182, 230)', height: '40px' ,width:'200px',fontSize:'20px'}}>注册</Button>
             </div>
-        )
-    }
+            </div>
+      </div>
+    )
+  }
 }
-const WrappedRegistrationForm = Form.create({ name: 'register' })(Register);
-export default WrappedRegistrationForm
